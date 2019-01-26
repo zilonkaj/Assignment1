44d8e99f5bee43e1d93e4a6930b74351e2b4778e
zilonkaj <zilonkaj@hotmail.com>
2019-01-26 12:53:28 -0500

    Add CrazyRandomSword class

---
+++ CrazyRandomSword.cpp
@@ -0,0 +1,8 @@
+/*
+ * File:   CrazyRandomSword.cpp
+ * Author: Joseph <zilonkaj@hotmail.com>
+ *
+ * Created on January 26, 2019
+ */
+
+#include "CrazyRandomSword.h"
---
+++ CrazyRandomSword.h
@@ -0,0 +1,17 @@
+/*
+ * File:   CrazyRandomSword.h
+ * Author: Joseph <zilonkaj@hotmail.com>
+ *
+ * Created on January 26, 2019
+ */
+
+#ifndef ASSIGNMENT1_CRAZYRANDOMSWORD_H
+#define ASSIGNMENT1_CRAZYRANDOMSWORD_H
+
+
+class CrazyRandomSword {
+
+};
+
+
+#endif //ASSIGNMENT1_CRAZYRANDOMSWORD_H

5d85b0d4cc5a042031ab02cecd02f62fd48daaad
zilonkaj <zilonkaj@hotmail.com>
2019-01-26 13:49:44 -0500

    Add LightningStaff class

---
+++ LightningStaff.cpp
@@ -0,0 +1,8 @@
+/*
+ * File:   LightningStaff.cpp
+ * Author: Joseph <zilonkaj@hotmail.com>
+ *
+ * Created on January 26, 2019
+ */
+
+#include "LightningStaff.h"
---
+++ LightningStaff.h
@@ -0,0 +1,19 @@
+/*
+ * File:   LightningStaff.cpp
+ * Author: Joseph <zilonkaj@hotmail.com>
+ *
+ * Created on January 26, 2019
+ */
+
+#ifndef ASSIGNMENT1_LIGHTNINGSTAFF_H
+#define ASSIGNMENT1_LIGHTNINGSTAFF_H
+
+#include "Weapon.h"
+#include <string>
+
+class LightningStaff {
+
+};
+
+
+#endif //ASSIGNMENT1_LIGHTNINGSTAFF_H

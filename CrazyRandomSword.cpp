d27f8f4343b4b1b0eefc8d7909ac765c801d32ad
zilonkaj <zilonkaj@hotmail.com>
2019-01-26 13:46:43 -0500

    Implement CrazyRandomSword::hit()

--- CrazyRandomSword.cpp
+++ CrazyRandomSword.cpp
@@ -6,3 +6,19 @@
  */
 
 #include "CrazyRandomSword.h"
+
+double CrazyRandomSword::hit(double armor) {
+    srand(time(0));                 // seed random number generator
+
+    double thirdOfArmor = armor / 3;
+
+    // ignoredArmor is random number between 2 - thirdArmor
+    int ignoredArmor = static_cast<int>(rand() % (thirdOfArmor - 1) + 2);
+
+    double damage = hitPoints - ignoredArmor;
+
+    if (damage < 0)
+        return 0;
+    else
+        return damage;
+}

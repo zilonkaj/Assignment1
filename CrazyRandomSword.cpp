75b3465dfac991fee8234ba7c8657d5dcfaa202f
zilonkaj <zilonkaj@hotmail.com>
2019-01-26 15:24:26 -0500

    Change thirdOfArmor to int
    Compiler was throwing an error since the modulo operator
    only works with integers, not doubles.

--- CrazyRandomSword.cpp
+++ CrazyRandomSword.cpp
@@ -10,7 +10,7 @@
 double CrazyRandomSword::hit(double armor) {
     srand(time(0));                 // seed random number generator
 
-    double thirdOfArmor = armor / 3;
+    int thirdOfArmor = armor / 3;
 
     // ignoredArmor is random number between 2 - thirdArmor
     int ignoredArmor = static_cast<int>(rand() % (thirdOfArmor - 1) + 2);

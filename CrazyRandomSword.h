73567684149a4105050024fd0092b494740566f4
zilonkaj <zilonkaj@hotmail.com>
2019-01-26 13:29:03 -0500

    Implement constructor
    Cstdlib and Ctime were included since they are necessary
    for random number generation.

--- CrazyRandomSword.h
+++ CrazyRandomSword.h
@@ -10,12 +10,21 @@
 
 #include "Weapon.h"
 #include <string>
+#include <cstdlib>
+#include <ctime>
 
 class CrazyRandomSword : public Weapon {
 public:
 
+    // Assign hitPoints to temporary value of 0 since real value will be calculated in constructor
     CrazyRandomSword : Weapon("Crazy random sword",0)
     {
+        /*  Random number generation method taken from Bob Myer's notes:
+            https://www.cs.fsu.edu/~myers/c++/notes/rand.html
+        */
+
+        srand(time(0));                 // seed random number generator
+        hitPoints = rand() % 94 + 7     // set hitPoints to random number between 7 - 100
     }
 
     virtual ~SimpleHammer() {};

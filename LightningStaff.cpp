a253e1088a8a3ec4df2df234b0607b16551df02b
zilonkaj <zilonkaj@hotmail.com>
2019-01-26 14:41:10 -0500

    Implement LightningStaff::hit()

--- LightningStaff.cpp
+++ LightningStaff.cpp
@@ -6,3 +6,21 @@
  */
 
 #include "LightningStaff.h"
+
+double LightningStaff::hit(double armor) {
+
+    double damage;
+
+    if (armor > 10)
+        damage = hitPoints - armor;
+    else if (armor > 0)
+        damage = (hitPoints - armor) * 2;   // Double damage on armor < 10
+    else
+        damage = 1000;                      // Kills if no armor
+
+
+    if (damage < 0)
+        return 0;
+    else
+        return damage;
+}

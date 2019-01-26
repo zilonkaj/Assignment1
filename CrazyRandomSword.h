143815a0769272a16600d7b8cb411d66739a074a
zilonkaj <zilonkaj@hotmail.com>
2019-01-26 13:26:59 -0500

    Add method declarations and includes

--- CrazyRandomSword.h
+++ CrazyRandomSword.h
@@ -8,9 +8,19 @@
 #ifndef ASSIGNMENT1_CRAZYRANDOMSWORD_H
 #define ASSIGNMENT1_CRAZYRANDOMSWORD_H
 
+#include "Weapon.h"
+#include <string>
 
-class CrazyRandomSword {
+class CrazyRandomSword : public Weapon {
+public:
 
+    CrazyRandomSword : Weapon("Crazy random sword",0)
+    {
+    }
+
+    virtual ~SimpleHammer() {};
+
+    virtual double hit(double armor);
 };
 
 
